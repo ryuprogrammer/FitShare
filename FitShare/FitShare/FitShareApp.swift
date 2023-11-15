c89e888a1771baad248d0aa732dfbd799fb9ef35
トム・クルーズ <tomukuruzu@tomukuruzunoMacBook-Air.local>
2023-11-15 19:30:05 +0900

    add file

---
+++ FitShare/FitShare/FitShareApp.swift
@@ -0,0 +1,17 @@
+//
+//  FitShareApp.swift
+//  FitShare
+//
+//  Created by トム・クルーズ on 2023/11/15.
+//
+
+import SwiftUI
+
+@main
+struct FitShareApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
